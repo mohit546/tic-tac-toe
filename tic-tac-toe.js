4b64b2e8cd77eb456b86be1dc8666959bdb76a16
Anonous <abcd546@gmail.com>
2018-06-15 14:07:12 +0530

    - computer code modified

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,80 +1,71 @@
-var board = [
-	[null, null, null],
-	[null, null, null],
-	[null, null, null]
-],
+var board = [[{}, {}, {}, {x: 0, o: 0}], [{}, {}, {}, {x: 0, o: 0}], [{}, {}, {}, {x: 0, o: 0}], [{x: 0, o: 0}, {x: 0, o: 0}, {x: 0, o: 0}, {x: 0, o: 0}]];
 boardSize = 3,
-count = 0;
+count = 0,
+digonal = {
+	one: ['00', '11', '22'],
+	two: ['02', '11', '20']
+};
+var isComputer = false;
 
 var sketchBoard = document.createElement('div');
 sketchBoard.id = 'sketchBoard';
 sketchBoard.style.cssText = 'position: absolute; top: 40%; left: 45%;';
 document.getElementsByTagName('body')[0].appendChild(sketchBoard);
 
+var checkDigonal = function(){
+	var x1 = x2 = o1 = o2 = 0;
+	for(var i = 0, l = digonal.one.length; i < l; i++){
+		if(board[digonal.one[i].charAt(0)][digonal.one[i].charAt(1)]['player'] == 'X'){ x1++; }
+		if(board[digonal.two[i].charAt(0)][digonal.two[i].charAt(1)]['player'] == 'X'){ x2++; }
+		if(board[digonal.one[i].charAt(0)][digonal.one[i].charAt(1)]['player'] == 'O'){ o1++; }
+		if(board[digonal.two[i].charAt(0)][digonal.two[i].charAt(1)]['player'] == 'O'){ o2++; }
+
+		if(x1 == 3 || x2 == 3) anounceWinner('X');
+		if(o1 == 3 || o2 == 3) anounceWinner('O');
+	}
+};
+
 var eventListener = function(e) {
 	if (e.target.innerText.length) return;
 	if (parseInt(count) % 2 === 0) {
 		this.innerText = 'X';
+		board[parseInt(this.id[0])][parseInt(this.id[1])]['player'] = 'X';
+		board[parseInt(this.id[0])][board.length - 1]['x']++;
+		board[board.length - 1][parseInt(this.id[1])]['x']++;
+		if (board[parseInt(this.id[0])][board.length - 1]['x'] == 3 || board[board.length - 1][parseInt(this.id[1])]['x'] == 3){
+			anounceWinner('X');
+		}
+		if (isComputer) computer();
 	} else {
 		this.innerText = 'O';
+		board[parseInt(this.id[0])][parseInt(this.id[1])]['player'] = 'O';
+		board[parseInt(this.id[0])][board.length - 1]['o']++;
+		board[board.length - 1][parseInt(this.id[1])]['o']++;
+		if (board[parseInt(this.id[0])][board.length - 1]['o'] == 3 || board[board.length - 1][parseInt(this.id[1])]['o'] == 3) {
+			anounceWinner('O');
+		}
 	}
+	checkDigonal();
 	count++;
 	if(count == 9){
 		setTimeout(function () { alert('Draw !!!'); resetBoard(); }, 100);
 	}
-	checkWinner();
+	console.log(board);
 }
 
 var drawBoard = function(){
-	for (var i = 0, l = board.length; i < l; i++) {
-		for (var j = 0, len = board[i].length; j < len; j++) {
+	for (var i = 0, l = board.length - 1; i < l; i++) {
+		for (var j = 0, len = board[i].length - 1; j < len; j++) {
 			var sqare = document.createElement('div');
 			sqare.id = i + '' + j;
 			sqare.style.cssText = 'width: 50px; height: 50px; border: 1px solid #aaa; display: inline-block; float: left; line-height: 1.5; text-align: center; font-size: 2em;';
 			sqare.addEventListener('click', eventListener);
 			document.getElementById('sketchBoard').appendChild(sqare);
+			board[i][j]['box'] = sqare;
 		}
 		document.getElementById('sketchBoard').appendChild(document.createElement('br'));
 	}
 }();
-
-var checkWinner = function(){
-	var verticalCheck1 = '';
-	var verticalCheck2 = '';
-	var verticalCheck3 = '';
-	var diagonalCheck1 = '';
-	var diagonalCheck2 = '';
-	var temp = 2;
-	for (var i = 0, l = board.length; i < l; i++) {
-		var horizontalCheck = '';
-		verticalCheck1 = verticalCheck1 + '' + document.getElementById(i + '' + 0).innerText;
-		verticalCheck2 = verticalCheck2 + '' + document.getElementById(i + '' + 1).innerText;
-		verticalCheck3 = verticalCheck3 + '' + document.getElementById(i + '' + 2).innerText;
-
-		diagonalCheck1 = diagonalCheck1 + '' + document.getElementById(i + '' + i).innerText;
-		diagonalCheck2 = diagonalCheck2 + '' + document.getElementById(i + '' + parseInt(temp-i)).innerText;
-
-		for (var j = 0, len = board[i].length; j < len; j++) {
-			horizontalCheck = horizontalCheck + '' + document.getElementById(i + '' + j).innerText;
-		}
-
-		getCount(horizontalCheck);
-		getCount(verticalCheck1);
-		getCount(verticalCheck2);
-		getCount(verticalCheck3);
-		getCount(diagonalCheck1);
-		getCount(diagonalCheck2);
-	}
-}
-
-var getCount = function (word){
-	if (word.length == 3) {
-		var count = word.split(word[0]).length - 1;
-		if (count == 3) {
-			anounceWinner(word[0]);
-		}
-	}
-}
 
 var anounceWinner = function(w){
 	setTimeout(function () { alert(w + ' win !!!'); resetBoard(); }, 100);
@@ -82,9 +73,84 @@ var anounceWinner = function(w){
 
 var resetBoard = function(){
 	count = 0;
-	for (var i = 0, l = board.length; i < l; i++) {
-		for (var j = 0, len = board[i].length; j < len; j++) {
+	board = [[{}, {}, {}, { x: 0, o: 0 }], [{}, {}, {}, { x: 0, o: 0 }], [{}, {}, {}, { x: 0, o: 0 }], [{ x: 0, o: 0 }, { x: 0, o: 0 }, { x: 0, o: 0 }, { x: 0, o: 0 }]];
+	for (var i = 0, l = board.length - 1; i < l; i++) {
+		for (var j = 0, len = board[i].length - 1; j < len; j++) {
 			document.getElementById(i + '' + j).innerText = '';
+			board[i][j]['box'] = document.getElementById(i + '' + j);
 		}
+	}
+};
+
+var computer = function(){
+	count++;
+	var xlistx = [];
+	var xlisty = [];
+	var olistx = [];
+	var olisty = [];
+	var xCountx = 0;
+	var xCounty = 0;
+	var oCountx = 0;
+	var oCounty = 0;
+	for(var i = 0, l = board.length-1; i < l; i++){
+		xlistx.push(board[i][l].x);
+		olistx.push(board[i][l].o);
+		xlisty.push(board[l][i].x);
+		olisty.push(board[l][i].o);
+		// for (var j = 0; j < l; j++){
+		// 	console.log(i, j, board[i][j]['player']);
+		// 	if(!board[i][j]['player']){
+		// 		board[i][j]['player'] = 'O';
+		// 		board[i][j]['box'].innerText = 'O';
+		// 		board[i][l]['o']++;
+		// 		board[l][j]['o']++;
+		// 		return;
+		// 	}
+		// }
+	}
+
+	var xMax = Math.max(...xlistx),
+		xMaxIndex = xlistx.indexOf(xMax);
+	var yMax = Math.max(...xlisty),
+		yMaxIndex = xlisty.indexOf(yMax);
+
+	console.log('xlistx list ---> ', xlistx);
+	console.log('xlisty list ---> ', xlisty);
+	console.log('olistx list ---> ', olistx);
+	console.log('olisty list ---> ', olisty);
+	console.log('xMax ---> ', xMax, xMaxIndex);
+	console.log('yMax ---> ', yMax, yMaxIndex);
+
+	// if (!board[i][j]['player']) {
+	// 	board[i][j]['player'] = 'O';
+	// 	board[i][j]['box'].innerText = 'O';
+	// 	board[i][l]['o']++;
+	// 	board[l][j]['o']++;
+	// 	return;
+	// }
+	var finalIndex = null;
+
+	for(var i = 0, l = 3; i < l; i++){
+		console.log('xMax ---> ', finalIndex);
+		// console.log('finalIndex ---> ', board[finalIndex][i]);
+		if (xMax > yMax){
+			finalIndex = xMaxIndex;
+			if (!board[finalIndex][i]['player']) {
+				board[finalIndex][i]['player'] = 'O';
+				board[finalIndex][i]['box'].innerText = 'O';
+				board[finalIndex][l]['o']++;
+				board[l][i]['o']++;
+				break;
+			}
+		} else{
+			finalIndex = yMaxIndex;
+			if (!board[i][finalIndex]['player']) {
+				board[i][finalIndex]['player'] = 'O';
+				board[i][finalIndex]['box'].innerText = 'O';
+				board[l][finalIndex]['o']++;
+				board[l][finalIndex]['o']++;
+				break;
+			}
+		};
 	}
 };
